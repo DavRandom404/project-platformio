08c8fdece8a03f96b1de5ac6cfe691954d0a4ed2
DavRandom404 <87178297+DavRandom404@users.noreply.github.com>
2021-07-15 12:01:49 +0100

    Fixed a lot, look in changes

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
     info.changeLifeBy(-1)
-    myCorg.startEffect(effects.fire, 500)
+    myCorg.sprite.startEffect(effects.fire, 500)
+    myCorg.sprite.setPosition(10, 70)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
     info.changeLifeBy(-1)
-    myCorg.startEffect(effects.fire)
+    myCorg.sprite.startEffect(effects.fire)
+    myCorg.sprite.setPosition(10, 70)
 })
 info.onLifeZero(function () {
     game.over(false)
 })
-let myCorg: Sprite = null
+let myCorg: Corgio = null
 info.setLife(3)
 scene.setBackgroundImage(assets.image`spice`)
 myCorg = corgio.create(SpriteKind.Player, 10, 70)
 myCorg.updateSprite()
-myCorg.follow()
-myCorg.verticalMovement()
+scene.cameraFollowSprite(myCorg.sprite)
 myCorg.horizontalMovement()
 tiles.setTilemap(tilemap`level1`)
+myCorg.verticalMovement()
